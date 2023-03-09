9d7e991ac9b74b0ab9b97b899d850e6ad3bf1b17
milorobles <ccamilomartinezr@gmail.com>
2023-03-09 00:21:25 -0500

    inicializamos el constructor

---
+++ src/Monedas.java
@@ -0,0 +1,19 @@
+
+
+public class Monedas {
+
+    //Atributos
+    
+    public String pesoColombiano;
+    public String pesoArgentino;
+    public String pesoChileno;
+    public String pesoMexicano;
+    public String dolar;
+
+    //constructor
+
+    
+
+
+    
+}
